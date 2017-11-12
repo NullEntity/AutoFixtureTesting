a7241b1cd094634d9e8e6b9bae05e9513476b6aa
Bryce Walther <bryce@brycewalther.com>
2017-11-12 16:50:12 -0500

    Add project files.

---
+++ AutoFixtureTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoFixtureTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoFixtureTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2fa1206a-5cb0-408b-b6fa-74e706b5c3a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutoFixtureTest/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+
+namespace AutoFixtureTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var fixture = new Fixture();
+            fixture.Customize(new CompositeCustomization(
+                new FooComponent(),
+                new BarComponent()
+            ));
+            var baz = fixture.Create<Baz>();
+
+            Assert.IsNull(baz.FooString, "Foo.FooString should be null");
+            Assert.AreEqual(baz.BarString, "bar", "Bar.BarString should be constant \"bar\"");
+            Assert.IsNotNull(baz.BazString, "Baz.BazString should be randomly generated");
+        }
+    }
+
+    public class FooComponent : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<Baz>(composer => composer.OmitAutoProperties());
+        }
+    }
+
+    public class BarComponent : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<Bar>(composer => composer.With(bar => bar.BarString, "bar"));
+        }
+    }
+
+    public class Foo
+    {
+        public string FooString;
+    }
+
+    public class Bar : Foo
+    {
+        public string BarString;
+    }
+
+    public class Baz : Bar
+    {
+        public string BazString;
+    }
+}
